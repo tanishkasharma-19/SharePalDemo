695c0c9a987312213688ca8f658517e3a5a19a84
tanishkasharma-19 <tanishkasharma2114@gmail.com>
2025-07-12 15:27:19 +0530

    Your commit message here

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -7,32 +7,43 @@ export default function Benefits() {
       <h2>Benefits</h2>
       <div className="benefit-scroll-wrapper">
         <div className="benefit-items">
+
           <div className="benefit-item">
-            <img src="/download (4).jpeg" alt="Planet" />
+            <img 
+              src="/Benefit1.jpeg" 
+              alt="1"/>
             <h3>Good for our Planet</h3>
             <p>
               Reduce waste, cut down on landfills,
               and lower your carbon footprint by renting.
             </p>
           </div>
+
           <div className="benefit-item">
-            <img src="/Benefit2.jpeg" alt="Quality" />
+            <img 
+              src="/Benefit2.jpeg" 
+              alt="2"/>
             <h3>Great for Adventure</h3>
             <p>
               Save big by renting instead of buying.
               Enjoy premium products without breaking the bank.
             </p>
           </div>
+
           <div className="benefit-item">
-            <img src="/Benefit3.jpeg" alt="Pocket Friendly" />
+            <img 
+              src="/Benefit3.jpeg" 
+              alt="3"/>
             <h3>Easy on Your Pocket</h3>
             <p>
               Save big by renting instead of buying.
               Enjoy premium products without breaking the bank.
             </p>
           </div>
+
         </div>
       </div>
     </section>
   );
 }
+
--- src/components/Mainpart.js
+++ src/components/Mainpart.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import CategoriesDropdown from './CategoriesDropdown';
 import './Mainpart.css';
 
  export default function Hero() {
@@ -7,7 +6,7 @@ import './Mainpart.css';
     <section className="hero">
       <h2>"Own the Experience, Rent the Gear"</h2>
       <p>Explore top-quality gear and share the joy of adventure.</p>
-      <CategoriesDropdown />
+    
     </section>
   );
 }
